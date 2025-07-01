78f9819d0da36bc4ca8a5476f8ddf82f30ae9094
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-07-01 13:05:34 +0200

    feat: snack 2 completato

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Snack 1
 function getPostTitle(id){
     return new Promise((resolve,reject)=>{
         fetch(`https://dummyjson.com/posts/${id}`)
@@ -8,3 +9,22 @@ function getPostTitle(id){
 }
 
 getPostTitle(1)
+
+// Snack 2
+
+function lanciaDado(){
+    return new Promise((resolve,reject)=>{
+        console.log('sto lanciando il dado...')
+        setTimeout(()=>{
+            const percentage = Math.round(Math.random() * 100)
+            const randomNumber = Math.round((Math.random() * 5) + 1)
+            if(percentage <= 20){
+                reject('Il dado si è incastrato')
+            }else{
+                resolve(console.log(randomNumber))
+            }
+        },3000)
+    })
+}
+
+lanciaDado()
